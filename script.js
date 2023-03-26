47e2b7a60c91e303fb6db7c8a52db731af4c2b9e
Payalbh20 <117173906+Payalbh20@users.noreply.github.com>
2023-03-26 10:06:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,59 +1,22 @@
-const app = require("./index.js");
-app.listen(5500, () => {
-  console.log('server started');
-});
-var timeLeft = document.querySelector(".display__time-left");
-var endTime = document.querySelector(".display__end-time");
-var input = document.querySelector("input");
+function myFunction() {
+const startingMins = document.getElementById(custom);
+let time = startingMins*60;
 
-var previousTimers = {};
-function startTimer(duration) {
-  var now = new Date();
+const countdownEl = document.getElementByClassName(display__time-left);
+const endtimeEl = document.getElementByClassName(display__end-time);
 
-  var timer = duration,
-    minutes,
-    seconds;
+let currentTime = new Date();	
+setInterval(updateCountdown, 1000);
 
-  now.setMinutes(now.getMinutes() + duration / 60);
-  var ampm = now.getHours() >= 12 ? "PM" : "AM";
+function updateCountdown(startingMins) {
+	const minutes = Math.floor(time/60);
+	let seconds = time%60;
 
-  endTime.textContent = `${
-    now.getHours() == "12" ? 12 : now.getHours() % 12
-  }:${now.getMinutes()} ${ampm}`;
+	seconds = seconds < 10 ? '0' + seconds : seconds;
 
-  if (previousTimers.timeId) {
-    window.clearInterval(previousTimers.timeId);
-  }
-  previousTimers.timeId = setInterval(function () {
-    minutes = parseInt(timer / 60, 10);
-    seconds = parseInt(timer % 60, 10);
-
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-
-    timeLeft.textContent = minutes + ":" + seconds;
-
-    timer--;
-    if (timer < 0) {
-      window.clearInterval(previousTimers.timeId);
-    }
-  }, 1000);
+	currentTime = currentTime + 
+	countdownEl.innerHTML = `${minutes} : ${seconds}`;
+	time--;
 }
 
-var form = document.querySelector("#custom");
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-});
-input.addEventListener("change", () => {
-  startTimer(input.value);
-});
-
-var allButtons = document.querySelectorAll("button");
-
-for (let button of allButtons) {
-  button.addEventListener("click", (e) => {
-    let target = e.target;
-    let time = target.attributes["data-time"].value;
-    startTimer(time);
-  });
 }
